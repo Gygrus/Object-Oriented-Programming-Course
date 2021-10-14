323aba28a15401edcc81b991ca56f304d47c1112
piotr <piotrsocala@student.agh.edu.pl>
2021-10-14 17:41:43 +0200

    punkt 15

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -2,25 +2,25 @@ package agh.ics.oop;
 import static java.lang.System.out;
 public class World {
     public static void main(String[] args){
-        System.out.println("System wystartował");
-        String[] tab = { "f", "b", "l", "r" };
+        System.out.println("Start");
+        String[] tab = { "f", "f", "r", "l" };
         run(tab);
-        System.out.println("System zakończył działanie");
+        System.out.println("Stop");
     }
     public static void run(String[] args){
         for(int i = 0; i < args.length; i++){
             switch(args[i]){
                 case "f":
-                    out.println("Do przodu");
+                    out.println("Zwierzak idzie do przodu");
                     break;
                 case "b":
-                    out.println("Do tyłu");
+                    out.println("Zwierzak idzie do tyłu");
                     break;
                 case "r":
-                    out.println("W prawo");
+                    out.println("Zwierzak skręca w prawo");
                     break;
                 case "l":
-                    out.println("W lewo");
+                    out.println("Zwierzak skręca w lewo");
                     break;
 
             }
