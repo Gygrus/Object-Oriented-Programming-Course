8a4dc5989b140cb42fc60d88a36b50df73f03ef8
piotr <piotrsocala@student.agh.edu.pl>
2021-10-21 16:33:19 +0200

    laby2

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -3,10 +3,20 @@ import static java.lang.System.out;
 public class World {
     public static void main(String[] args){
         out.println("Start");
-        String[] tab = { "f", "f", "r", "l" };
+        String[] tab = args;
         Direction[] tab1 = string_to_enum(tab);
         run(tab1);
         out.println("Stop");
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
+        MapDirection check = MapDirection.EAST;
+        out.println(check.next());
+        out.println(check.previous());
+        out.println(check.toUnitVector());
+        out.println(check.toString());
     }
     public static void run(Direction[] args){
         for(int i = 0; i < args.length; i++){
